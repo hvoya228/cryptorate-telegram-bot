44b1c8de4b125881a2395dafe49b27d6f0979a55
Chugaister <bogucukij@gmail.com>
2023-09-10 20:01:22 +0300

    initial

---
+++ CryptoRateController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptocurrencyRateBot
+{
+    public class CryptoRateController
+    {
+        public async Task<string> GetPingInfo()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string apiUrl = "https://api.coingecko.com/api/v3/ping";
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return "Server connected";
+                    }
+                    else
+                    {
+                        return "Server is not responding";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message;
+                }
+            }
+        }
+
+        public async Task<string> GetCryptoRate(string cryptoName, string currencyName)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string apiUrl = $"https://api.coingecko.com/api/v3/simple/price?ids={cryptoName}&vs_currencies={currencyName}";
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        return json;
+                    }
+                    else
+                    {
+                        return response.StatusCode.ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using CryptocurrencyRateBot;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+var botClient = new TelegramBotClient("5787517537:AAGiO3mfftfgYBx4F_mIrf5SfWS5JrKBoTI");
+var cryptoRateController = new CryptoRateController();
+
+using CancellationTokenSource cts = new();
+
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>()
+};
+
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+  
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    if (update.Message is not { } message)
+        return;
+
+    if (message.Text is not { } messageText)
+        return;
+
+    var chatId = message.Chat.Id;
+
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+    if (message.Text is "/ping")
+    {
+        string pingInfo = await cryptoRateController.GetPingInfo();
+
+        Message sentPingInfo = await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: pingInfo,
+            cancellationToken: cancellationToken);
+    }
+    else if (message.Text is "/help")
+    {
+        Message sentHelp = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "This bot can send you the rate of any cryptovalue! \n" +
+                      "To see rate, write this: \"/coinName_currencyName\" \n" +
+                      "See server status: \"/ping\"",
+                cancellationToken: cancellationToken);
+    }
+    else if (message.Text.Contains("_"))
+    {
+        string[] parts = message.Text.Split('_');
+
+        if (parts.Length == 2)
+        {
+            string cryptoName = parts[0].Substring(1);
+            string currencyName = parts[1];
+
+            Console.WriteLine(cryptoName);
+            Console.WriteLine(currencyName);
+
+            string rate = await cryptoRateController.GetCryptoRate(cryptoName, currencyName);
+
+            if (rate is "{}")
+            {
+                Message sentCoinNameError = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Smth went wrong!",
+                cancellationToken: cancellationToken);
+            }
+            else
+            {
+                Message sentRate = await botClient.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: rate,
+                    cancellationToken: cancellationToken);
+            }
+        }
+    }
+    else
+    {
+        Message sentCheckHelp = await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "Check /help to see how it works!",
+            cancellationToken: cancellationToken);
+    }
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
